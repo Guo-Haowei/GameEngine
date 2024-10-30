69c9498aaf7bf8c1f38a2b85f3718251ec3bf289
Haowei Guo <haguo@wizcorp.jp>
2024-10-31 01:14:49 +0900

    face camera

--- source/shader/hlsl/particle.vert.hlsl
+++ source/shader/hlsl/particle.vert.hlsl
@@ -9,19 +9,33 @@ float4 main(vsinput_position input, uint instance_id
     float tz = transform.z;
     float s = transform.w;
 
-    // float4x4 world_matrix = float4x4(
-    //     s, 0.0, 0.0, 0.0,
-    //     0.0, s, 0.0, 0.0,
-    //     0.0, 0.0, s, 0.0,
-    //     tx, ty, tz, 1.0);
-    float4x4 world_matrix = float4x4(
-        s, 0.0, 0.0, tx,
-        0.0, s, 0.0, ty,
-        0.0, 0.0, s, tz,
+    float4x4 scale_matrix = float4x4(
+        s, 0.0, 0.0, 0.0,
+        0.0, s, 0.0, 0.0,
+        0.0, 0.0, s, 0.0,
         0.0, 0.0, 0.0, 1.0);
 
-    float4 position = mul(world_matrix, float4(input.position, 1.0));
-    position = mul(g_view_matrix, position);
+    float4x4 model_matrix = float4x4(
+        1.0, 0.0, 0.0, tx,
+        0.0, 1.0, 0.0, ty,
+        0.0, 0.0, 1.0, tz,
+        0.0, 0.0, 0.0, 1.0);
+
+    // make sure always face to screen
+    // https://blog.42yeah.is/opengl/rendering/2023/06/24/opengl-billboards.html
+    float4x4 view_model_matrix = mul(g_view_matrix, model_matrix);
+    view_model_matrix[0][0] = 1.0;
+    view_model_matrix[0][1] = 0.0;
+    view_model_matrix[0][2] = 0.0;
+    view_model_matrix[1][0] = 0.0;
+    view_model_matrix[1][1] = 1.0;
+    view_model_matrix[1][2] = 0.0;
+    view_model_matrix[2][0] = 0.0;
+    view_model_matrix[2][1] = 0.0;
+    view_model_matrix[2][2] = 1.0;
+
+    float4 position = mul(scale_matrix, float4(input.position, 1.0));
+    position = mul(view_model_matrix, position);
     position = mul(g_projection_matrix, position);
     return position;
 }
