d71ae33fd5368a95650e34821aedfece5758a853
Haowei Guo <haguo@wizcorp.jp>
2024-11-24 17:00:42 +0900

    WIP: error

--- source/engine/tests/engine/core/error/error.test.cpp
+++ source/engine/tests/engine/core/error/error.test.cpp
@@ -6,6 +6,60 @@
 
 namespace my {
 
+using Error = InternalError<ErrorCode>;
+using ErrorRef = std::shared_ptr<Error>;
+
+template<typename T>
+using Result = std::expected<T, ErrorRef>;
+
+template<typename T>
+[[nodiscard]] static inline constexpr auto _create_error_arg_1(std::string_view p_file,
+                                                               std::string_view p_function,
+                                                               int p_line,
+                                                               T p_value) {
+    return std::unexpected(std::shared_ptr<InternalError<T>>(new InternalError<T>(p_file, p_function, p_line, p_value)));
+};
+
+template<typename T>
+[[nodiscard]] static inline constexpr auto _create_error_arg_1(std::string_view p_file,
+                                                               std::string_view p_function,
+                                                               int p_line,
+                                                               std::shared_ptr<InternalError<T>>& p_error) {
+    return std::unexpected(std::shared_ptr<InternalError<T>>(new InternalError<T>(p_file, p_function, p_line, p_error)));
+};
+
+template<typename T, typename... Args>
+[[nodiscard]] static inline constexpr auto _create_error_arg_2_plus(std::string_view p_file,
+                                                                    std::string_view p_function,
+                                                                    int p_line,
+                                                                    T p_value,
+                                                                    std::format_string<Args...> p_format,
+                                                                    Args&&... p_args) {
+    return std::unexpected(std::shared_ptr<InternalError<T>>(new InternalError<T>(p_file, p_function, p_line, p_value, p_format, std::forward<Args>(p_args)...)));
+};
+
+// @TODO: move to a more general place
+#define HBN_MACRO_EXPAND(x)                                        x
+#define HBN_GET_MACRO_2(_1, _2, FUNC, ...)                         FUNC
+#define HBN_GET_MACRO_3(_1, _2, _3, FUNC, ...)                     FUNC
+#define HBN_GET_MACRO_4(_1, _2, _3, _4, FUNC, ...)                 FUNC
+#define HBN_GET_MACRO_5(_1, _2, _3, _4, _5, FUNC, ...)             FUNC
+#define HBN_GET_MACRO_6(_1, _2, _3, _4, _5, _6, FUNC, ...)         FUNC
+#define HBN_GET_MACRO_7(_1, _2, _3, _4, _5, _6, _7, FUNC, ...)     FUNC
+#define HBN_GET_MACRO_8(_1, _2, _3, _4, _5, _6, _7, _8, FUNC, ...) FUNC
+
+#define HBN_ERROR_1(_1)                             _create_error_arg_1<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1)
+#define HBN_ERROR_2(_1, _2)                         _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2)
+#define HBN_ERROR_3(_1, _2, _3)                     _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2, _3)
+#define HBN_ERROR_4(_1, _2, _3, _4)                 _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2, _3, _4)
+#define HBN_ERROR_5(_1, _2, _3, _4, _5)             _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2, _3, _4, _5)
+#define HBN_ERROR_6(_1, _2, _3, _4, _5, _6)         _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2, _3, _4, _5, _6)
+#define HBN_ERROR_7(_1, _2, _3, _4, _5, _6, _7)     _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2, _3, _4, _5, _6, _7)
+#define HBN_ERROR_8(_1, _2, _3, _4, _5, _6, _7, _8) _create_error_arg_2_plus<ErrorCode>(__FILE__, __FUNCTION__, __LINE__, _1, _2, _3, _4, _5, _6, _7, _8)
+
+#undef HBN_ERROR
+#define HBN_ERROR(...) HBN_MACRO_EXPAND(HBN_GET_MACRO_8(__VA_ARGS__, HBN_ERROR_8, HBN_ERROR_7, HBN_ERROR_6, HBN_ERROR_5, HBN_ERROR_4, HBN_ERROR_3, HBN_ERROR_2, HBN_ERROR_1)(__VA_ARGS__))
+
 TEST(InternalError, constructor_no_string) {
     constexpr size_t LINE_NUMBER = __LINE__;
     auto err = HBN_ERROR(OK).error();
@@ -20,6 +74,14 @@ TEST(InternalError, constructor_with_format) {
     EXPECT_EQ(err->line, LINE_NUMBER + 1);
     EXPECT_EQ(err->value, ERR_ALREADY_EXISTS);
     EXPECT_EQ(err->message, "(1=2=3)");
+}
+
+TEST(InternalError, constructor_with_long_format) {
+    constexpr size_t LINE_NUMBER = __LINE__;
+    auto err = HBN_ERROR(ERR_ALREADY_EXISTS, "({}={}={}={}={}={})", 1, -2, 3, 5.5f, 'c', "def").error();
+    EXPECT_EQ(err->line, LINE_NUMBER + 1);
+    EXPECT_EQ(err->value, ERR_ALREADY_EXISTS);
+    EXPECT_EQ(err->message, "(1=-2=3=5.5=c=def)");
 }
 
 static Result<void> DoStuffImpl() {
